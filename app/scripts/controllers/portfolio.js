a14b80b1cf9d1d4c26d58f08abfa3b380478db59
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-07-15 20:59:36 -0500

    ability to add orgs saved to firebase

--- app/scripts/controllers/portfolio.js
+++ app/scripts/controllers/portfolio.js
@@ -8,10 +8,25 @@
  * Controller of the angularfireApp
  */
 angular.module('angularfireApp')
-  .controller('PortfolioCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('PortfolioCtrl', function ($scope, Ref, $firebaseArray, $timeout) {
+    // synchronize a read-only, synchronized array of organizations, limit to most recent 10
+    $scope.organizations = $firebaseArray(Ref.child('organizations').limitToLast(10));
+
+    // display any errors
+    $scope.organizations.$loaded().catch(alert);
+
+    // provide a method for adding a message
+    $scope.addOrganization = function() {
+        // push an organization to the end of the array
+        $scope.organizations.$add({portion:1})
+          // display any errors
+          .catch(alert);
+    }
+
+    function alert(msg) {
+      $scope.err = msg;
+      $timeout(function() {
+        $scope.err = null;
+      }, 5000);
+    }
   });
